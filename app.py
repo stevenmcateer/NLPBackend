4fc45420f1d79420eaf6222d883d712d85e2263b
Jesse Gaulin <linuxboss182@gmail.com>
2019-02-25 12:35:13 -0500

    Added database for students, stores the group they are in so they stay in the same group if they revisit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func 
 from numpy import random
+import model
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -12,8 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 question = 'This is a question'
-responseCount=0;
-
 
 class questionResponses(db.Model):
     responseId = db.Column(db.Integer, primary_key=True)
@@ -27,8 +26,28 @@ class questionResponses(db.Model):
     def __repr__(self):
         return f"questionResponses('{self.problemId}', '{self.questionId}', '{self.response}')"
 
+class students(db.Model):
+    studentId = db.Column(db.String(60), primary_key=True, nullable=False)
+    group = db.Column(db.String(60)) #0 False in experiement, 1 True in control
+    
+    def __repr__(self):
+        return f"students('{self.studentId}', '{self.group}')"
 
+def findGroup(studentId):
+    if not studentId:
+        return random.choice([0, 1])
 
+    control = random.choice(['0', '1'])
+    student = students.query.filter_by(studentId=studentId).all()
+
+    if not student: #Save this students group if first time seen him
+        student = students(group=control, studentId=studentId)
+        db.session.add(student)
+        db.session.commit()
+    else: #Get this students group
+        control = student[0].group
+
+    return control
 
 class PostResponse(FlaskForm):
     content = TextAreaField('Answer', validators=[DataRequired()])
@@ -54,36 +73,36 @@ def home():
     problemId = request.args.get('p4')
     questionId = request.args.get('p5')
 
-    submit=False
-    #this is temporty will need to create infustructure to get which group student is in
-    control = random.choice([True, False])
+    control = findGroup(userReference)
+    submit = False
     form = PostResponse()
+
     if form.validate_on_submit():
         if form.grade.data:
-            grade = random.randint(4)
-            global responseCount
+            grade = model.calculateGrade(questionId)
             message = 'Your rough estimated score is a ' + str(grade)
            # Need to store this into into a db for each student/question 
            # message2 =  'This is attempt #: ' +  str(responseCount)
            # flash(message2, 'success')
            # responseCount = responseCount + 1
-            temp = questionResponses.query.filter_by(studentId=1, questionId=1 ).all()
-            responseId = db.session.query(func.max(questionResponses.responseId)).scalar() + 1
+           # temp = questionResponses.query.filter_by(studentId=1, questionId=1 ).all()
+            if(problemId):
+                responseId = db.session.query(func.max(questionResponses.responseId)).scalar() + 1
+                response = questionResponses(responseId=responseId,studentId=userReference, problemId=problemId,questionId=questionId,response=form.content.data, grade=grade)
+                db.session.add(response)
+                db.session.commit()
 
-            response = questionResponses(responseId=responseId,studentId=userReference, problemId=problemId,questionId=questionId,response=form.content.data, grade=grade)
-            db.session.add(response)
-            db.session.commit()
         else:
-            message = 'Your response has been saved! Please hit next peoblem to continue'
-            
-            responseId = db.session.query(func.max(questionResponses.responseId)).scalar() + 1
-            response = questionResponses(responseId=responseId,studentId=userReference, problemId=problemId,questionId=questionId,response=form.content.data)
-            db.session.add(response)
-            db.session.commit()
-            
-            submit=True
+            message = 'Your response has been saved! Please hit next problem to continue'
+            if(problemId):
+                responseId = db.session.query(func.max(questionResponses.responseId)).scalar() + 1
+                response = questionResponses(responseId=responseId,studentId=userReference, problemId=problemId,questionId=questionId,response=form.content.data)
+                db.session.add(response)
+                db.session.commit()
+            submit = True
+
         flash(message, 'success')
-    return render_template('question.html', question=question, form=form, submit=submit, control = control)
+    return render_template('question.html', question=question, form=form, submit=submit, control=control)
 
 
 
---
+++ model.py
@@ -0,0 +1,6 @@
+from numpy import random
+
+
+def calculateGrade(question):
+    return random.randint(4)
+
