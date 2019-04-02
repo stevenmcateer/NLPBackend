2d6880380bbb85e1d1e366a23d9c5a9f5399ebcc
Jesse Gaulin <linuxboss182@gmail.com>
2019-04-02 16:26:17 -0400

    Super simple model implementation

--- model.py
+++ model.py
@@ -2,5 +2,17 @@ from numpy import random
 
 
 def calculateGrade(question):
-    return random.randint(4)
+    grade = 4
+    question_lower = question.lower()
+
+    if 'battery' not in question_lower and 'voltage source' not in question_lower:
+        grade = grade - 1
+
+    if 'wire' not in question_lower and 'conductor' not in question_lower and 'closed circuit' not in question_lower:
+        grade = grade - 1
+
+    if 'resistor' not in question_lower and 'load' not in question_lower and 'light bulb' not in question_lower:
+        grade = grade - 1
+
+    return grade
 
